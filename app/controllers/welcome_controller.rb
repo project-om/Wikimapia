cc0f3e9cdccde82dfec7a7ef413f3a1a0b985e17
Juan <zhcns.nj@gmail.com>
2018-01-16 11:54:11 -0500

    added search function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,17 @@ class WelcomeController < ApplicationController
   def test
     @response = HTTParty.get("http://api.wikimapia.org/?key=#{ENV['wikimapia_api_key']}&function=category.getall&name=Checkpoint&format=json") 
     end
+
+  def search
+    base_url = "http://api.wikimapia.org/"
+
+    request_params = {:format => "json", :key => ENV["wikimapia_api_key"], :function => "place.search" }
+  
+    request_params.merge!(search_params)
+    @response = HTTParty.get base_url, query: request_params 
+  end  
+
+  def search_params
+  	params.permit([:q, :lat, :lon, :distance])
+  end 
 end
