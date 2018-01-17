f68b96f023fe1eb90c0ae12350ae031fa915c39c
Juan <zhcns.nj@gmail.com>
2018-01-17 17:44:57 -0500

    created instance variables in Index method welcome controller, created index view page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,32 @@
 class WelcomeController < ApplicationController
 
   def index
+     
+       #@response = HTTParty.get("http://api.wikimapia.org/?key=#{ENV['wikimapia_api_key']}&function=place.getnearest&lat=35.95451&lon=39.00934&format=json")
+      base_url = "http://api.wikimapia.org/"
+
+      request_params = {:format => "json", :key => ENV["wikimapia_api_key"], :function => "place.getnearest" }
+  
+      request_params.merge!(search_params)
+      @response = HTTParty.get base_url, query: request_params 
+
+      @results = {}
+      @results[:query] = response['name']
+      @results[:distance] = response['meters']
   end
 
-  def test
-    @response = HTTParty.get("http://api.wikimapia.org/?key=#{ENV['wikimapia_api_key']}&function=category.getall&name=Checkpoint&format=json") 
-    end
+  #def test
+    #@response = HTTParty.get("http://api.wikimapia.org/?key=#{ENV['wikimapia_api_key']}&function=place.getnearest&lat=35.95451&lon=39.00934&format=json") 
+  #end
 
-  def search
-    base_url = "http://api.wikimapia.org/"
+  #def search
+    #base_url = "http://api.wikimapia.org/"
 
-    request_params = {:format => "json", :key => ENV["wikimapia_api_key"], :function => "place.search" }
+    #request_params = {:format => "json", :key => ENV["wikimapia_api_key"], :function => "place.getnearest" }
   
-    request_params.merge!(search_params)
-    @response = HTTParty.get base_url, query: request_params 
-  end  
+    #request_params.merge!(search_params)
+    #@response = HTTParty.get base_url, query: request_params 
+  #end  
 
   def search_params
   	params.permit([:q, :lat, :lon, :distance])
